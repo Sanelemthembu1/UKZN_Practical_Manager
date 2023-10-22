f48c9a63b39440f0ea2f4c3653d7a76b43901b18
Sanelemthembu1 <112512341+Sanelemthembu1@users.noreply.github.com>
2023-10-22 02:34:35 +0200

    5th Commit

--- src/adminPracSec.java
+++ src/adminPracSec.java
@@ -1,8 +1,4 @@
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.common.PDRectangle;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
+
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
